b42c49c3acc1f7bd399f1346c2cbbe31334e0e75
Alejandro Alcalde <algui91@gmail.com>
2015-04-29 13:27:33 +0200

    P2: Added ej2

--- P2/streamciphers/ej2.py
+++ P2/streamciphers/ej2.py
@@ -9,27 +9,17 @@ def LFSR(coef,sem,longitud):
     
     assert(len(coef)==len(sem))
         
-    fin_cadena = ""
-    fin_lista = []
+    l = ""
     
-    #Por cada caracter a generar
     for k in xrange(0,longitud):
-        #reiniciamos el feedback
         sj = 0
-        #por cada coeficiente del polinomio
         for i in xrange(0,len(coef)):
-            #se multiplica (and) y se hace el xor con la anterior sj
             a = sem[i] & coef[i]
             sj = sj ^ a
-        #se anade la semilla a la cadena
-        fin_cadena += str(sem[len(sem)-1])        
-        fin_lista.append(int(sem[len(sem)-1]))
-        #se anade al principio
-        sem = [sj]+sem
-        # y se desplaza la semilla
-        sem = sem[0:len(sem)-1]
-
+        l += (str(sem[len(sem) - 1]))
+        sem = [sj] + sem
+        sem = sem[:len(sem) - 1]
  
-    return fin_cadena,fin_lista
+    return l
     
-print LFSR([0,1,1,0,0], [1,1,1,1,1], 100)
+print LFSR([1,0,1], [1,1,1], 24)

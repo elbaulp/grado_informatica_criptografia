25161ed57d1c341800a3c20caf08b311714f9e8d
Alejandro Alcalde <algui91@gmail.com>
2015-05-03 17:44:46 +0200

    P2: NLFSR

---
+++ P2/streamciphers/ej3.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 17:17:26 2015
+
+@author: hkr (elbauldelprogramador.com)
+"""
+
+def NLFSR(f, s, k):
+
+    assert(len(f[0])==len(s))
+
+    l = []
+    
+    for _ in range(0,k): 
+        sj = 0
+        for i in f:
+            r = 1
+            for j,seed in zip(i,s):
+                r = (r + seed * j) % 2
+            sj = sj ^ r
+            
+        l.append(sj)
+        s = [sj] + s
+        
+        s = s[:-1]
+
+    return l
+    
+print NLFSR([[0, 0, 0, 0], [1, 1, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]], [1, 0, 1, 1], 20)
--- P2/streamciphers/ej4.py
+++ P2/streamciphers/ej4.py
@@ -49,7 +49,7 @@ def decrypt(c, k):
     return m
     
 c,k = encrypt(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "MI BESO",
             [1,1,0,0,1,0], [1,1,1,1,0,1],
             [1,0,1,0,1,1], [1,0,1,1,1,1],
             [1,1,0,1,0,0], [1,1,0,1,0,0])

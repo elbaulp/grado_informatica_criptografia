e1d948e74c3c1800491862132868f6746d8be4cf
Alejandro Alcalde <algui91@gmail.com>
2015-06-28 21:27:27 +0200

    Added option to choose where to store keys

--- P3/oneway/ej7.py
+++ P3/oneway/ej7.py
@@ -25,9 +25,59 @@ PUB_KEY = PATH + "rsa.pub"
 PRIV_KEY = PATH + "rsa.priv"
 SIGNED_FILE = PATH + "signed.bin"
 
+def main():
+    parser = argparse.ArgumentParser(description='Generate RSA keys, sign and verify signed files')
+    parser.add_argument('-d','--directory', help='Where to store the keys and signature', type=str, metavar='<path>', required=True)
+    parser.add_argument('-g','--genkeys', help='Generate RSA keys', type=int, metavar='<key size>')
+    parser.add_argument('-s', '--sign', help='Sign a file', type=str, nargs=2, metavar=('<file>', '<priv key>'))
+    parser.add_argument('-v', '--verify', help='Verify a signed file', type=str, nargs=3, metavar=('<original file>', '<signed file>', '<pub key>'))
+
+    args = parser.parse_args()
+
+    if args.genkeys == None and args.sign == None and args.verify == None:
+        parser.print_help()
+    else:
+        global PATH
+        global PUB_KEY
+        global PRIV_KEY
+        global SIGNED_FILE
+
+        PATH = args.directory
+        PUB_KEY = PATH + "rsa.pub"
+        PRIV_KEY = PATH + "rsa.priv"
+
+        if args.genkeys:
+            print "Generating keys..."
+            gen_keys(args.genkeys)
+            print "Done, keys placed in " + PATH
+            print "Your public key:"
+            ascii_print(PUB_KEY)
+            print "Your private key:"
+            ascii_print(PRIV_KEY)
+        elif args.sign:
+            print "Signing file..."
+            SIGNED_FILE = PATH + args.sign[0] + ".signed"
+            sign(args.sign[0], args.sign[1])
+            print "Sign placed in " + SIGNED_FILE + " here is the sign"
+            ascii_print(SIGNED_FILE)
+        elif args.verify:
+            print "Verifying signature..."
+            check, original = verify_sign(args.verify[0], args.verify[1], args.verify[2])
+            if check == original:
+                print "File is correctly signed"
+            else:
+                warning = "## INVALID SIGNATURE!!! ##"
+                print
+                print "#"*len(warning)
+                print warning
+                print "#"*len(warning)
+                print
+            print "Original signature: \n" + str(original)
+            print "Current signature \n" + str(check)
+
 def get_prime(p):
     """Returns the next prime starting in the number p
-    
+
     :param p: The number from when to start looking for pimes
     :returns: The next prime starting from p
     """
@@ -51,16 +101,16 @@ def gen_keys(size=1024):
         n1 = n1 + 1
     if not 0x1 & n2:
         n2 = n2 + 1
-        
+
     p = get_prime(n1)
     q = get_prime(n2)
-    
+
     n = p * q
     phi_n = (p-1)*(q-1)
     e = compute_e(phi_n)
     # Compute the private key, d = e^-1 mod (phi_n)
     d = moduloInverse(e, phi_n)
-    
+
     ascii_n = binascii.b2a_base64(str(n))
     with open(PUB_KEY, 'wb') as file:
         ascii_e = binascii.b2a_base64(str(e))
@@ -77,7 +127,7 @@ def sign(input_file=None, priv_key=None):
     :param priv_key: The private RSA key to use when signing the file
     :returns: A Signature placed in a file in PATH
     """
-    
+
     if input_file == None or priv_key == None:
         print "You must enter a file to sign and a private key"
         exit
@@ -87,15 +137,15 @@ def sign(input_file=None, priv_key=None):
             n = int(binascii.a2b_base64(file.readline()))
         with open(input_file, 'rb') as file:
             h = int(sha512(file.read()).hexdigest(),16)
-            
+
         with open(SIGNED_FILE, 'wb') as file:
             ascii_sign = binascii.b2a_base64(str(powerModInt(h, d, n)))
             file.write(ascii_sign)
-        
+
 def verify_sign(original_file=None, signature=None, pub_key=None):
     """Verify that a signature is valid, given the original signed file, the signature
     and the public key.
-    
+
     :param original_file: The file which was signed
     :param signature: The signature made when the file was signed
     :pub_key: The RSA public key to verify that the file was signed correctly
@@ -110,50 +160,11 @@ def verify_sign(original_file=None, signature=None, pub_key=None):
             e = int(binascii.a2b_base64(file.readline()))
         with open(signature, 'rb') as file:
             sign = int(binascii.a2b_base64(file.readline()))
-            
+
         with open(original_file, 'rb') as file:
             original = int(sha512(file.read()).hexdigest(), 16) % n
-        
-        return powerModInt(sign, e, n), original
 
-def main():
-    parser = argparse.ArgumentParser(description='Generate RSA keys, sign and verify signed files')
-    parser.add_argument('-g','--genkeys', help='Generate RSA keys', type=int, metavar='<key size>')
-    parser.add_argument('-s', '--sign', help='Sign a file', type=str, nargs=2, metavar=('<file>', '<priv key>'))
-    parser.add_argument('-v', '--verify', help='Verify a signed file', type=str, nargs=3, metavar=('<original file>', '<signed file>', '<pub key>'))
-    
-    args = parser.parse_args()
-    
-    if args.genkeys == None and args.sign == None and args.verify == None:
-        parser.print_help()
-    else:
-        if args.genkeys:
-            print "Generating keys..."
-            gen_keys(args.genkeys)
-            print "Done, keys placed in " + PATH
-            print "Your public key:"
-            ascii_print(PUB_KEY)
-            print "Your private key:"
-            ascii_print(PRIV_KEY)
-        elif args.sign:
-            print "Signing file..."
-            sign(args.sign[0], args.sign[1])
-            print "Sign placed in " + SIGNED_FILE + " here is the sign"
-            ascii_print(SIGNED_FILE)
-        elif args.verify:
-            print "Verifying signature..."
-            check, original = verify_sign(args.verify[0], args.verify[1], args.verify[2])
-            if check == original:
-                print "File is correctly signed"
-            else:
-                warning = "## INVALID SIGNATURE!!! ##"
-                print
-                print "#"*len(warning)
-                print warning
-                print "#"*len(warning)
-                print
-            print "Original signature: \n" + str(original)
-            print "Current signature \n" + str(check)
+        return powerModInt(sign, e, n), original
 
 if __name__ == '__main__':
     main()
